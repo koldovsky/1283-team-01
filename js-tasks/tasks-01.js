90bb3e68d5a57ff5e574233eeb9da1fd2cb55bda
Nastya Berest <112124602+anastasiiaxfr@users.noreply.github.com>
2024-10-18 09:53:55 +0300

    Added task tasks-01.js

--- js-tasks/tasks-01.js
+++ js-tasks/tasks-01.js
@@ -4,3 +4,21 @@ function bonusTime(salary, bonus) {
   return "£" + (bonus ? salary * 10 : salary);
 }
 
+//https://www.codewars.com/kata/5264d2b162488dc400000001/javascript
+function spinWords(string) {
+  //TODO Have fun :)
+  const str = string.split(" ");
+  const new_str = [];
+
+  str.forEach(function (el) {
+    if (el.length >= 5) {
+      new_str.push(el.split("").reverse().join(""));
+    } else {
+      new_str.push(el);
+    }
+  });
+
+  return new_str.join(" ");
+}
+
+//spinWords("Hey fellow warriors");
