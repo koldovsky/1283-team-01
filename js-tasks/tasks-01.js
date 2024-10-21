e29acc7d29b7741f871c4b9bc23b2c2db4442938
Borys <93729317+Botisichh@users.noreply.github.com>
2024-10-21 14:20:16 +0000

    Added task for js

--- js-tasks/tasks-01.js
+++ js-tasks/tasks-01.js
@@ -22,3 +22,34 @@ function spinWords(string) {
 }
 
 //spinWords("Hey fellow warriors");
+
+
+// Messi Goals:
+function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+}
+
+// Make negative:
+function makeNegative(num) {
+   return num > 0 ? -num : num;
+}
+
+// Game Move:
+function move(position, roll) {
+  return position + roll * 2;
+}
+
+// Personalized Message:
+function greet(name, owner) {
+  return name === owner ? 'Hello boss' : 'Hello guest'
+}
+
+// Keep Hydrated:
+function litres(time) {
+  return Math.floor(time * 0.5);
+}
+
+ // Opposites Attract:
+function lovefunc(flower1, flower2){
+  return (flower1 % 2 !== flower2 % 2);
+}
