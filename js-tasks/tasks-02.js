c719e984f075019d2531662330dde46d07166efd
Nastya Berest <112124602+anastasiiaxfr@users.noreply.github.com>
2024-10-22 06:36:11 +0000

    tsk3

--- js-tasks/tasks-02.js
+++ js-tasks/tasks-02.js
@@ -3,3 +3,15 @@ function stringToArray(string){
     const stringArr = string.split(" ");
     return stringArr;
 }
+
+// https://www.codewars.com/kata/577a98a6ae28071780000989/train/javascript
+var min = function(list){
+    list.sort((a, b) => a - b);
+    return list[0];
+}
+
+var max = function(list){
+    list.sort((a, b) => b - a);
+    return list[0];
+}
+
