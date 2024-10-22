67df1e225a739ca36e0747fee734df82f88fcbeb
Nastya Berest <112124602+anastasiiaxfr@users.noreply.github.com>
2024-10-22 06:59:52 +0000

    tsk2

--- js-tasks/tasks-02.js
+++ js-tasks/tasks-02.js
@@ -29,3 +29,18 @@ function min(arr, toReturn) {
 
     return toReturn === "value" ? smallestValue : smallestIndex;
 }
+
+// https://www.codewars.com/kata/dna-to-rna-conversion/train/javascript
+function DNAtoRNA(dna) {
+    let rna = '';
+
+    for (let i = 0; i < dna.length; i++) {
+        if (dna[i] === 'T') {
+            rna += 'U';
+        } else {
+            rna += dna[i];
+        }
+    }
+
+    return rna;
+}
