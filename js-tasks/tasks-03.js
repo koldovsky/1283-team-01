a26ac461fa9811a67250c42507fb7b214a7eed37
Nastya Berest <112124602+anastasiiaxfr@users.noreply.github.com>
2024-10-25 06:27:26 +0000

    tsk2

--- js-tasks/tasks-03.js
+++ js-tasks/tasks-03.js
@@ -2,3 +2,10 @@
 class GetMinNumb {
   findSmallestInt = args => Math.min(...args);
 }
+
+// https://www.codewars.com/kata/geometry-basics-circle-circumference-in-2d/train/javascript
+function circleCircumference(circle) {
+  const radius = circle.radius;
+  const circumference = 2 * Math.PI * radius;
+  return Number(circumference.toFixed(6));
+}
