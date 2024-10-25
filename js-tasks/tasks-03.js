c0a9015a7245056a02c1228741496b1a20d77e91
Nastya Berest <112124602+anastasiiaxfr@users.noreply.github.com>
2024-10-25 06:34:03 +0000

    tsk3

--- js-tasks/tasks-03.js
+++ js-tasks/tasks-03.js
@@ -9,3 +9,17 @@ function circleCircumference(circle) {
   const circumference = 2 * Math.PI * radius;
   return Number(circumference.toFixed(6));
 }
+
+// https://www.codewars.com/kata/training-js-number-12-loop-statement-for-dot-in-and-for-dot-of/train/javascript
+function giveMeFive(obj) {
+  const arr = [];
+  for (let key in obj) {
+      if (key.length === 5) {
+          arr.push(key);
+      }
+      if (obj[key].length === 5) {
+          arr.push(obj[key]);
+      }
+  }
+  return arr;
+}
