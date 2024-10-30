0f4669aba8156220e71a1f07f8b9582fd599a021
analevus <analevus6@gmail.com>
2024-10-30 16:28:08 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 function init() {
     import('./header-menu.js');
+    import('./aboutus.js');
 }
 
 const totalPartials = document.querySelectorAll('[hx-trigger="load"], [data-hx-trigger="load"]').length;
