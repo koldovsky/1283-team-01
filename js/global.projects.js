e8c3cd63e4fdd0769fa12b7b4791ec7a1d9a86b9
Denis <89018768+DenisChepel@users.noreply.github.com>
2024-10-31 00:18:18 +0000

    Added galery to global.projects

--- js/global.projects.js
+++ js/global.projects.js
@@ -1,3 +1,4 @@
+//Carousel
 function loadModuleFromCDN(url, callback) {
     const script = document.createElement('script');
     script.type = 'module';
@@ -30,4 +31,49 @@ window.onload = function() {
     });
 
     document.body.addEventListener('htmx:afterSwap', initAllEmblas);
+};
+
+
+//Galery
+const modal = document.getElementById('projects__imageModal');
+const modalImage = document.getElementById('projects__modalImage');
+const closeModal = document.querySelector('.projects__close');
+const modalPrev = document.querySelector('.projects__modal-prev');
+const modalNext = document.querySelector('.projects__modal-next');
+
+let currentImageIndex = 0;
+let currentImages = [];
+
+function openModal(imageSrc, images) {
+    modal.style.display = 'flex';
+    modalImage.src = imageSrc;
+    currentImages = images;
+    currentImageIndex = currentImages.indexOf(imageSrc);
+}
+
+closeModal.onclick = function() {
+    modal.style.display = 'none';
+}
+
+document.querySelectorAll('.projects__image').forEach((image) => {
+    image.onclick = function() {
+        const images = Array.from(image.closest('.projects__carousel').querySelectorAll('.projects__image')).map(img => img.src);
+        openModal(image.src, images);
+    };
+});
+
+modalNext.onclick = function() {
+    currentImageIndex = (currentImageIndex + 1) % currentImages.length; // loop back to first image
+    modalImage.src = currentImages[currentImageIndex];
+};
+
+modalPrev.onclick = function() {
+    currentImageIndex = (currentImageIndex - 1 + currentImages.length) % currentImages.length; // loop back to last image
+    modalImage.src = currentImages[currentImageIndex];
+};
+
+modal.onclick = function(event) {
+    if (event.target === modal) {
+        modal.style.display = 'none';
+    }
 };
