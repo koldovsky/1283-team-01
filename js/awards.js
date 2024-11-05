7c36c7f8a71242fd5fb56810ac377c2aaa261738
TitovAndrey1996 <titovandrey1996@gmail.com>
2024-11-05 06:13:32 +0000

    fix awards.js

--- js/awards.js
+++ js/awards.js
@@ -1,13 +1,13 @@
-console.log("awards.js is loaded and running");
+// console.log("awards.js is loaded and running");
 
 setTimeout(() => {
     document.querySelectorAll('.awards__item').forEach(item => {
         item.addEventListener('mouseover', () => {
-            console.log('Mouse over award item');
+            // console.log('Mouse over award item');
             item.classList.add('hovered');
         });
         item.addEventListener('mouseout', () => {
-            console.log('Mouse out of award item');
+            // console.log('Mouse out of award item');
             item.classList.remove('hovered');
         });
     });
